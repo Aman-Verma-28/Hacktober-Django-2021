6866a251775e6c94ee20c62630a5f5b02dcc706a
Mohit <mamohitagarwal40@gmail.com>
2021-10-09 19:45:22 +0530

    adding the designing file in python

---
+++ Begineer Friendly/desiging.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+
+def print_rangoli(size):
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    subset = alpha[:size]
+    base = "-".join(reversed(subset))
+    rows = []
+    for i in range(size):
+        row = base[:len(base) - i*2]
+        row = ("-" * (len(base) - len(row))) + row
+        rows.insert(0, row)
+    rows.extend(reversed(rows[:-1]))
+    for i in range(len(rows)):
+        row = rows[i]
+        rev_row = row[::-1]
+        rows[i] = row + rev_row[1:]
+    print(*rows, sep="\n")
