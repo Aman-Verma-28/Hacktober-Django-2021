b4e42b4e66f94e05323058980f038fd3198d4d3d
Akarsh Anand <akarshanand2810@gmail.com>
2021-10-09 18:14:07 +0530

    Added Code1 in java

---
+++ Hactober Code/Code1.java
@@ -0,0 +1,22 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class AppendReplacementExample
+{
+	public static void main(String[] args)
+	{
+		//AppendReplacementExample peobj = new AppendReplacementExample();
+		
+		Pattern pattern = Pattern.compile("John");
+		
+		Matcher matcher = pattern.matcher("John does this, and John does that");
+		
+		StringBuffer s1 = new StringBuffer();
+		
+		while(matcher.find())
+		{
+			matcher.appendReplacement(s1,"sam");
+			System.out.println(s1.toString());
+		}
+	}
+}
